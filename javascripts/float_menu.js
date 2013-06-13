b397838b2c8b03482ba1530f2fea78ad97348674
Laurent Doguin <ldoguin@nuxeo.com>
2013-06-13 19:57:56 +0200

    fix floating menu position

--- javascripts/float_menu.js
+++ javascripts/float_menu.js
@@ -11,13 +11,14 @@
             $(item).html('<a name="anchor-'+i+'">'+$(item).html()+'</a>');
         });
         $mainContainer.append($menuContainer);
-
-       
+        var t = jQuery("#welcome").offset().top + 20;
+        $mainContainer.animate({top:t},{duration:0,queue:false}); 
         $(window).scroll(function () {
+           
             if(!menuYloc)
                 menuYloc = parseInt($mainContainer.css("top").substring(0,$mainContainer.css("top").indexOf("px")));
             var offset = menuYloc+$(document).scrollTop()+"px";  
-            $mainContainer.animate({top:offset},{duration:500,queue:false});  
+            $mainContainer.animate({top:offset},{duration:0,queue:false});  
         }); 
         $('body').append($mainContainer);
     });
