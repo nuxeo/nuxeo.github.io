d633a3797118bfdcecb6578e0999e0f7bb41903e
Laurent Doguin <ldoguin@nuxeo.com>
2013-06-13 19:41:51 +0200

    add lightbox

---
+++ javascripts/lightbox.js
@@ -0,0 +1,37 @@
+// Lightbox
+;(function() {
+
+
+    $(document).ready(function () { 
+
+      $('img').click(function (e) { 
+
+        e.preventDefault();
+        var image_href = $(this).attr("src");
+        if ($('#lightbox').length > 0) { 
+            $('#content').html('<img src="' + image_href + '" />');
+            //show lightbox window - you could use .show('fast') for a transition
+            $('#lightbox').show();
+        }
+        else {
+            var lightbox =
+            '<div id="lightbox">' +
+                '<p>Click to close</p>' +
+                '<div id="content">' + //insert clicked link's href into img src
+                    '<img src="' + image_href +'" />' +
+                '</div>' +
+            '</div>';
+            $('body').append(lightbox);
+        }
+
+
+      });
+      $('#lightbox').live('click', function() { 
+        $('#lightbox').hide();
+      });
+      $(document).keyup(function(e) {
+        $('#lightbox').hide();
+      });
+    });
+
+}());
