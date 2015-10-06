f16448846368467a71ac8049307973cb2344d275
Julien Carsique <jcarsique@nuxeo.com>
2015-10-06 13:45:31 +0200

    add missing index.js

---
+++ javascripts/index.js
@@ -0,0 +1,21 @@
+function index(page, target) {
+    var div = $('<div/>');
+    div.load(page + " h2,h3");
+    div.ready(function() {
+        var h3 = false;
+        var items = "";
+        $.each(div.find("h2,h3"), function(i, item) {
+            if ($(item).prop('tagName') === "H3") {
+                if (!h3) {
+                    items += '<ul id="' + i + '">';
+                    h3 = true;
+                }
+            } else if (h3) {
+                items += '</ul>';
+                h3 = false;
+            }
+            items += '<li><a href="' + page + '#anchor-' + i + '">' + $(item).text() + '</a></li>';
+        });
+        $(target).html(items);
+    });
+}
