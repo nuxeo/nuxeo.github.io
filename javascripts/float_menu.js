f413ca3b768a4902591e145b4a77c1e7aa20b20e
Julien Carsique <jcarsique@nuxeo.com>
2015-10-06 12:42:03 +0200

    automate index and menu generation based on h2,h3

--- javascripts/float_menu.js
+++ javascripts/float_menu.js
@@ -1,25 +1,44 @@
 // Floating Menu
-;(function() {
-
-
-    $(document).ready(function () { 
-        var  $mainContainer = $("<div id='floatMenu'>");
+;
+(function() {
+    $(document).ready(function() {
+        var $mainContainer = $("<div id='floatMenu'>");
         var menuYloc;
         $menuContainer = $("<ul>");
-        $.each( $("h2"), function (i,item) {
-            $menuContainer.append('<li><a href="#anchor-'+i+'">'+$(item).text()+'</a></li>');
+        $.each($("h2,h3"), function(i, item) {
+            var offset = "";
+            if ($(item).prop('tagName') === "H3") {
+                offset = "&nbsp;&nbsp;";
+            }
+            $menuContainer.append('<li><a href="#anchor-' + i + '">' + offset + $(item).text() + '</a></li>');
+            $(item).prepend('<a id="anchor-' + i + '"></a>');
         });
         $mainContainer.append($menuContainer);
         var t = jQuery("#welcome").offset().top + 21;
-        $mainContainer.animate({top:t},{duration:0,queue:false}); 
-        $(window).scroll(function () {
-           if ($(document).scrollTop() > t) {
-             var offset = $(document).scrollTop()+"px";  
-             $mainContainer.animate({top:offset},{duration:0,queue:false});  
-           } else {
-             $mainContainer.animate({top:t},{duration:0,queue:false});  
-           }
-        }); 
+        $mainContainer.animate({
+            top : t
+        }, {
+            duration : 0,
+            queue : false
+        });
+        $(window).scroll(function() {
+            if ($(document).scrollTop() > t) {
+                var offset = $(document).scrollTop() + "px";
+                $mainContainer.animate({
+                    top : offset
+                }, {
+                    duration : 0,
+                    queue : false
+                });
+            } else {
+                $mainContainer.animate({
+                    top : t
+                }, {
+                    duration : 0,
+                    queue : false
+                });
+            }
+        });
         $('body').append($mainContainer);
     });
 
