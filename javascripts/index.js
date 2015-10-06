5736c811f5032b892fad3eb0086a9d1a961f8433
Julien Carsique <jcarsique@nuxeo.com>
2015-10-06 15:09:15 +0200

    fix index dynamic load

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,26 +1,25 @@
 function index(page, target) {
     var div = $('<div/>');
-    div.load(page + " h2,h3", function(response, status, xhr) {
+    div.load(page, function(response, status, xhr) {
         if (status == "error") {
             var msg = "Sorry but there was an error: ";
             console.error(msg + xhr.status + " " + xhr.statusText);
-        }
-    });
-    div.ready(function() {
-        var h3 = false;
-        var items = "";
-        $.each(div.find("h2,h3"), function(i, item) {
-            if ($(item).prop('tagName') === "H3") {
-                if (!h3) {
-                    items += '<ul id="' + i + '">';
-                    h3 = true;
+        } else {
+            var h3 = false;
+            var items = "";
+            $(response).find("h2,h3").each(function(i, item) {
+                if ($(item).prop('tagName') === "H3") {
+                    if (!h3) {
+                        items += '<ul id="' + i + '">';
+                        h3 = true;
+                    }
+                } else if (h3) {
+                    items += '</ul>';
+                    h3 = false;
                 }
-            } else if (h3) {
-                items += '</ul>';
-                h3 = false;
-            }
-            items += '<li><a href="' + page + '#anchor-' + i + '">' + $(item).text() + '</a></li>';
-        });
-        $(target).html(items);
+                items += '<li><a href="' + page + '#anchor-' + i + '">' + $(item).text() + '</a></li>';
+            });
+            $(target).html(items);
+        }
     });
 }
